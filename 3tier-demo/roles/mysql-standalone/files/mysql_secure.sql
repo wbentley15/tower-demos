0cadd09a9008265b0249db96666c8e8913908780
Walter Bentley <wbentley@wbentley-osx.attlocal.net>
2017-06-08 20:32:17 -0500

    .

---
+++ 3tier-demo/roles/mysql-standalone/files/mysql_secure.sql
@@ -0,0 +1,4 @@
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
